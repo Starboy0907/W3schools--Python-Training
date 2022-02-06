0ee48e59e22a1572e91d0445b1631816e3daa671
Starboy0907 <95019285+Starboy0907@users.noreply.github.com>
2022-02-06 05:25:29 -0800

    Completion of file system

---
+++ 6-python_data_types/data_types_exercises.py
@@ -0,0 +1,51 @@
+# Exercise1:
+# The following code example would print the data type of x, what data type would that be?
+# x = 5
+# print(type(x))
+
+# Answer: int
+
+# Exercise2:
+# The following code example would print the data type of x, what data type would that be?
+# x = "Hello World"
+# print(type(x))
+
+# Answer: string
+
+# Exercise3:
+# The following code example would print the data type of x, what data type would that be?
+# x = 20.5
+# print(type(x))
+
+#Answer: float
+
+# Exercise4:
+# The following code example would print the data type of x, what data type would that be?
+# x = ["apple", "banana", "cherry"]
+# print(type(x))
+
+#Answer: list
+
+# Exercise5:
+# The following code example would print the data type of x, what data type would that be?
+# x = ("apple", "banana", "cherry")
+# print(type(x))
+
+# Answer: tuple
+
+# Exercise6:
+# The following code example would print the data type of x, what data type would that be?
+# x = {"name" : "John", "age" : 36}
+# print(type(x))
+
+# Answer: dict
+
+# Exercise7:
+# The following code example would print the data type of x, what data type would that be?
+# x = True
+# print(type(x))
+
+# Answer: Bool
+
+
+
