d39e6e0bcd656ca4320457544eb005e2602d944e
Starboy0907 <95019285+Starboy0907@users.noreply.github.com>
2022-02-06 05:26:46 -0800

    file systems complete

---
+++ 4-python_comments/comments_exercise.py
@@ -0,0 +1,11 @@
+#Exercise1:
+#Comments in Python are written with a special character, which one?
+#This is a comment (hashtag)
+
+#Exercise2:
+#Use a multiline string to make the a multi line comment:
+
+"""This is a comment
+written in 
+more that just one line"""
+
---
+++ 5-python_variables/7-variable_exercises/variable_exercises.py
@@ -0,0 +1,46 @@
+# Exercise1:
+# Create a variable named carname and assign the value Volvo to it.
+
+# carname= "Volvo"
+
+# Exercise2:
+# Create a variable named x and assign the value 50 to it.
+
+# x = 50 
+
+# Exercise3:
+# Display the sum of 5 + 10, using two variables: x and y.
+
+# x = 5 
+# y = 10
+# print(x + y)
+
+# Exercise4:
+# Create a variable called z, assign x + y to it, and display the result.
+
+# x = 5
+# y = 10
+# z = x + y
+# print(z)
+
+# Exercise5:
+# Remove the illegal characters in the variable name:
+# 2my-first_name = "John"
+
+# Answer: myfirst_name = "John"
+
+# Exercise6:
+# Insert the correct syntax to assign the same value to all three variables in one code line.
+
+#Answer:
+# x = y = z = "Orange"
+
+# Exercise7:
+# Insert the correct keyword to make the variable x belong to the global scope.
+
+# Answer:
+# def myfunc():
+#     global x
+#     x = "fantastic"
+
+
