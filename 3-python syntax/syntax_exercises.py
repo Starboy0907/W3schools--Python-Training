dd684dfb5dd121ab5f6cdf73a80046f44d264387
Starboy0907 <95019285+Starboy0907@users.noreply.github.com>
2022-02-06 05:26:21 -0800

    File system complete

--- 3-python syntax/syntax_exercises.py
+++ 3-python syntax/syntax_exercises.py
@@ -1,7 +1,15 @@
-# Exercise:
+# Exercise1:
 # Insert the missing part of the code below to output "Hello World".
-
-
 # ("Hello World")
 
 # Answer: print('Hello World')
+
+# Exercise 2:
+# This example misses indentations to be correct.
+# Insert the missing indentation to make the code correct:
+# if 5 > 2:
+# print("Five is greater than two!")
+
+# Answer: 
+# if 5 > 2:
+#    print("Five is greater than two!")
