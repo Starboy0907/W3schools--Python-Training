335aed35e72ee4a0b781090da689661d24a2caf2
Starboy0907 <95019285+Starboy0907@users.noreply.github.com>
2022-02-12 17:36:55 -0800

    functions

---
+++ 12-python_lists/python_lists.py
@@ -0,0 +1,19 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+#Exercise2:
+#In the loop, when the item value is "banana", jump directly to the next item.
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        
+        print(x)
+
+for x in range(6):
+    print(x)
+
+
---
+++ 19-python_functions/functions_execises.py
@@ -0,0 +1,32 @@
+#Exercise:
+#Create a function named my_function.
+def my_function():
+    print("Hello from a function")
+
+#Exercise2:
+#Create a function named my_function.:
+print("Hello from a function")
+
+#Exercise3
+#Inside a function with two parameters, print the first parameter.
+def my_function(fname, lname):
+    print(fname)
+
+#Exercise4
+#If you do not know the number of arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix?
+def my_function(kids):
+    print("The youngest child is " + kids[2])
+
+#Exercise5
+#If you do not know the number of arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix?
+def my_function(*kids):
+    print("The younge9st child is " + kids[2])
+
+#Exercise6
+#If you do not know the number of keyword arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix?
+def my_function(**kid):
+    print("His last name is " + kid["lname"])
+my_function(fname="ZULU", lname="Shaka")
+
+
+
