5506ef62a774046fc37c54b4e3b3771afa9cbb4a
Starboy0907 <95019285+Starboy0907@users.noreply.github.com>
2022-02-20 23:45:17 -0800

    Classes and Objects

---
+++ 22-python_classes_objects/classes_objects_exercises.py
@@ -0,0 +1,18 @@
+#Exercise 1: 
+#Create a class named MyClass:
+
+class MyClass:
+    x = 5
+
+#Exercise 2
+
+#Create an object of MyClass called p1
+class MyClass:
+    x = 5
+p1= MyClass()
+print(p1.x)
+
+
+
+
+
